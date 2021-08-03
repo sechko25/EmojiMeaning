3acd233a0b4d3720362dda88c0c32f89b73ad5b1
secha <sechko5525@gmail.com>
2021-08-03 13:07:02 +0300

    lessons 1-4

---
+++ EmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by secha on 3.08.21.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
---
+++ EmojiReader/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  EmojiReader
+//
+//  Created by secha on 3.08.21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
