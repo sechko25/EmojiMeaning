d57dfe4b31588f7af60e0dc073dfbb53a36e960c
secha <sechko5525@gmail.com>
2021-08-18 12:17:21 +0300

    lesson 8

---
+++ EmojiReader/NewEmojiTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewEmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by secha on 18.08.21.
+//
+
+import UIKit
+
+class NewEmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
